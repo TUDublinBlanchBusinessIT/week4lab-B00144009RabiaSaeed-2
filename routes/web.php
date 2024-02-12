e9d6014775f2e6c1881de68c8a67b5713f16b689
B00144009 Rabia Saeed <b00144009@mytudublin.ie>
2024-02-12 16:28:36 +0000

    part one is now completed

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/customers/new', 'App\Http\Controllers\CustomerController@new');
+Route::post('/customers/create', 'App\Http\Controllers\CustomerController@create')->name('customers.create');
 
 
 Route::resource('members', App\Http\Controllers\memberController::class);
